6101fd75445466926f61350755b4943d7e1cad54
Manju.P <109751828+Manju63599@users.noreply.github.com>
2025-06-15 19:40:45 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -200,7 +200,7 @@ BEGIN TRY
 		cntry
 		)
 		SELECT
-		REPLACE(cid, '-','_') AS cid,
+		REPLACE(cid, '_','') AS cid,
 		CASE WHEN TRIM(cntry)= 'DE' THEN 'Germany'
 			 WHEN TRIM(cntry) IN ('US' ,'USA') THEN 'United States'
 			 WHEN TRIM(cntry)= ' ' OR cntry IS NULL THEN 'N/A'
